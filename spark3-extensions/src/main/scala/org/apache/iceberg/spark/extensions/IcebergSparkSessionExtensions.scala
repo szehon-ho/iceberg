c7d13212c1897be9eff891d8175077394309412e
Russell_Spitzer <rspitzer@apple.com>
2021-01-05 11:31:17 -0600

    Internal: Add an empty session extensions class in Spark
    Users may copy from OSS Iceberg instructions or blogs which will cite
    using IcebergSparkSessionExtensions which is not required in Apple Spark.
    To fix this we add in an empty class so they will not hit ClassNotFound
    exceptions.

---
+++ spark3-extensions/src/main/scala/org/apache/iceberg/spark/extensions/IcebergSparkSessionExtensions.scala
@@ -0,0 +1,31 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iceberg.spark.extensions
+
+import org.apache.spark.sql.SparkSessionExtensions
+import org.slf4j.LoggerFactory
+
+class IcebergSparkSessionExtensions extends (SparkSessionExtensions => Unit) {
+  private val LOG = LoggerFactory.getLogger(this.getClass)
+
+  override def apply(extensions: SparkSessionExtensions): Unit = {
+    LOG.warn("{} is set as an extension to Spark but this is not required with Apple Spark", this.getClass.getName)
+  }
+}
