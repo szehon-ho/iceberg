fa29c76595b22b438b384cbe9f8654efa25f6c93
Yufei Gu <yufei@apache.org>
2021-03-30 10:51:36 -0700

    Spark: Add table location to reserved table properties (#2330)
    Co-authored-by: Yufei Gu <yufei_gu@apple.com>

--- spark3/src/main/java/org/apache/iceberg/spark/source/SparkTable.java
+++ spark3/src/main/java/org/apache/iceberg/spark/source/SparkTable.java
@@ -64,7 +64,7 @@ public class SparkTable implements org.apache.spark.sql.connector.catalog.Table,
   private static final Logger LOG = LoggerFactory.getLogger(SparkTable.class);
 
   private static final Set<String> RESERVED_PROPERTIES = Sets.newHashSet(
-      "provider", "format", "current-snapshot-id", "sort-order");
+      "provider", "format", "current-snapshot-id", "location", "sort-order");
   private static final Set<TableCapability> CAPABILITIES = ImmutableSet.of(
       TableCapability.BATCH_READ,
       TableCapability.BATCH_WRITE,
@@ -139,6 +139,7 @@ public class SparkTable implements org.apache.spark.sql.connector.catalog.Table,
     String currentSnapshotId = icebergTable.currentSnapshot() != null ?
         String.valueOf(icebergTable.currentSnapshot().snapshotId()) : "none";
     propsBuilder.put("current-snapshot-id", currentSnapshotId);
+    propsBuilder.put("location", icebergTable.location());
 
     if (!icebergTable.sortOrder().isUnsorted()) {
       propsBuilder.put("sort-order", Spark3Util.describe(icebergTable.sortOrder()));
